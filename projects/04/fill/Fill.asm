f886655bd32606523a4812c0dd5ac8c272ce51d7
Aki <mifukia1101@gmail.com>
2018-02-03 00:14:14 +0900

    Complete Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,110 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+    @SCREEN  // get keyboard addres
+    D=A
+    @addr 
+    M=D 
+
+
+    @8192 // screen's last row 
+    D=A
+    @n
+    M=D
+
+    @i 
+    M=0
+
+    @KBD // input 
+    D=M
+    @WHITE // if keyboard input == 0 goto white screen
+    D;JEQ
+
+(BLACK)
+    //if i == n then loop again
+    @i 
+    D=M 
+    @n 
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    // paint black color
+    @addr
+    A=M   //change address to screen 
+    M=-1  // change RAM[addr] = 1111111111111111 black
+    
+    @i 
+    M=M+1
+    @addr
+    M=M+1
+    @BLACK
+    0;JMP 
+
+(WHITE)
+    //if i == n then loop again
+    @i 
+    D=M 
+    @n 
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    // paint white color
+    @addr
+    A=M   //change address to screen 
+    M=0  // change RAM[addr] = 000000000000 white
+    
+    @i 
+    M=M+1
+    @addr
+    M=M+1
+    @WHITE
+    0;JMP 
+
+
+
+    @LOOP
+    0;JMP
+
+
+
+//    @SCREEN
+//    D=A 
+//    @addr
+//    M=D 
+//
+//    @0
+//    D=M 
+//    @n 
+//    M=D 
+//
+//    @i 
+//    M=0
+//
+//(LOOP)
+//    @i 
+//    D=M 
+//    @n 
+//    D=D-M 
+//    @END 
+//    D;JGT 
+//
+//    @addr 
+//    A=M 
+//    M=-1 
+//
+//    @i 
+//    M=M+1 
+//    @32 
+//    D=A 
+//    @addr 
+//    M=D+M 
+//    @LOOP 
+//    0;JMP 
+//
+//(END)
+//    @END 
+//    0;JMP 
