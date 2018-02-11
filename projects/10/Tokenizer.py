a946c5515e9ec586e04e4b6e196f8c8396c508c5
Aki <mifukia1101@gmail.com>
2018-02-11 23:09:40 +0900

    before expressions

--- projects/10/Tokenizer.py
+++ projects/10/Tokenizer.py
@@ -14,23 +14,16 @@ IDENTIFIER = []
 # print(SYMBOLS)
 # SEPARATORS = r'\s'
 
-class JackTokenizer:
-    arithmetics = ["add","sub","neg","eq","gt","lt","and","or","not"]
-    commands = {
-        "arithmetic":"C_ARITHMETIC",\
-        "push":"C_PUSH",\
-        "pop":"C_POP",\
-        "label":"C_LABEL",\
-        "goto":"C_GOTO",\
-        "if-goto":"C_IF",\
-        "function":"C_FUNCTION",\
-        "return":"C_RETURN",\
-        "call":"C_CALL"
-        }
-
-
+# def writeLine(output_file,l):
+#     output_file.write(l)
+#     output_file.write("\n")
     
-    # from collections import deque
+def getValue(s):
+    return re.sub(r"\s?<.+?>\s?","",s)
+
+# print(getValue("<aaa> yattaze </aaa>"))
+
+class JackTokenizer:
     def __init__(self, file):
         # self.current_token = ""
         self.idx = 0
@@ -116,6 +109,10 @@ class JackTokenizer:
             return "&lt;"
         if self.current_token == ">":
             return "&gt;"
+        if self.current_token == "\"":
+            return "&quot;"
+        if self.current_token == "&":
+            return "&amp;"
         return self.current_token
 
     def identifier(self):
@@ -128,6 +125,299 @@ class JackTokenizer:
         return self.current_token[1:-1]
 
 
+
+class CompilationEngine:
+    def __init__(self,input,output):
+        self.input = input
+        self.output = open(output,'w')
+        self.idx = 0
+    
+    def close(self):
+        if self.output:
+            self.output.close()
+            self.output = None
+    
+    def check(self):
+        return self.idx < len(self.input)
+    
+    def CompileClass(self):
+        self.output.write("<class>\n")
+        self.output.write(self.input[self.idx] + "\n") # class 
+        self.idx += 1
+        self.output.write(self.input[self.idx] + "\n") # classname 
+        self.idx += 1
+        self.output.write(self.input[self.idx] + "\n") # symbol {
+        self.idx += 1
+
+        while getValue(self.input[self.idx]) in ["field","static"]:
+            self.CompileClassVarDec()
+            # print(self.idx)
+        while getValue(self.input[self.idx]) in ["constructor","function","method"]:
+            self.CompileSubroutine()
+
+        self.output.write(self.input[self.idx] + "\n") # symbol }
+        self.output.write("</class>\n")
+        return 0
+
+    # for field declaration
+    def CompileClassVarDec(self): 
+        if not self.check():
+            return 
+
+        self.output.write("<classVarDec>\n")
+        self.output.write(self.input[self.idx] + "\n") # static or field
+        self.idx += 1
+        self.output.write(self.input[self.idx] + "\n") # type
+        self.idx += 1
+        
+        while getValue(self.input[self.idx]) != ";":
+            self.output.write(self.input[self.idx] + "\n") # varname or ,
+            self.idx += 1
+
+        self.output.write(self.input[self.idx] + "\n") # ;
+        self.idx += 1
+        self.output.write("</classVarDec>\n")
+  
+        return 0
+    
+    # for method or something
+    def CompileSubroutine(self): 
+        if not self.check():
+            return
+
+        self.output.write("<subroutineDec>\n")
+        self.output.write(self.input[self.idx] + "\n") # constructor method function
+        self.idx += 1
+        self.output.write(self.input[self.idx] + "\n") # type
+        self.idx += 1
+        self.output.write(self.input[self.idx] + "\n") # subroutine name
+        self.idx += 1
+        self.output.write(self.input[self.idx] + "\n") # (
+        self.idx += 1
+
+        #paramlist
+        self.CompileParameterList()
+
+        self.output.write(self.input[self.idx] + "\n") # )
+        self.idx += 1
+
+        self.output.write(self.input[self.idx] + "\n") # {
+        self.idx += 1
+
+        #subroutine body => var* statements
+        #vardec
+        while getValue(self.input[self.idx]) == "var":
+            self.CompileVarDec()
+        # statements
+        self.CompileStatements()
+
+        self.output.write(self.input[self.idx] + "\n") # }
+        self.idx += 1
+        self.output.write("</subroutineDec>\n")
+
+        return 0
+    
+    # for fiedeclaration
+    def CompileParameterList(self): 
+        if not self.check():
+            return
+        self.output.write("<parameterList>\n")
+
+        self.output.write(self.input[self.idx] + "\n") # type
+        self.idx += 1
+        self.output.write(self.input[self.idx] + "\n") # varName
+        self.idx += 1
+        while getValue(self.input[self.idx]) == ",":
+            self.output.write(self.input[self.idx] + "\n") # ,
+            self.idx += 1
+            self.output.write(self.input[self.idx] + "\n") # type
+            self.idx += 1
+            self.output.write(self.input[self.idx] + "\n") # varName
+            self.idx += 1
+        
+        self.output.write("</parameterList>\n")
+        return 0
+    
+    def CompileVarDec(self): 
+        if not self.check():
+            return
+        self.output.write(self.input[self.idx] + "\n") # var
+        self.idx += 1
+        self.output.write(self.input[self.idx] + "\n") # type
+        self.idx += 1
+        self.output.write(self.input[self.idx] + "\n") # varName
+        self.idx += 1
+
+        while getValue(self.input[self.idx]) == ",":
+            self.output.write(self.input[self.idx] + "\n") # ,
+            self.idx += 1
+            self.output.write(self.input[self.idx] + "\n") # varName
+            self.idx += 1
+
+        self.output.write(self.input[self.idx] + "\n") # ;
+        self.idx += 1
+        
+        return 0
+
+    def CompileStatements(self): 
+        if not self.check():
+            return
+        self.output.write("<statements>\n")
+        if getValue(self.input[self.idx]) == "let":
+            self.CompileLet()
+        elif getValue(self.input[self.idx]) == "if":
+            self.CompileIf()
+        elif getValue(self.input[self.idx]) == "while":
+            self.CompileWhile()
+        elif getValue(self.input[self.idx]) == "do":
+            self.CompileDo()
+        elif getValue(self.input[self.idx]) == "return":
+            self.CompileReturn()
+
+        self.output.write("</statements>\n")
+
+        return 0
+
+    def CompileDo(self): 
+        if not self.check():
+            return
+        self.output.write("<doStatement>\n")
+
+        self.output.write(self.input[self.idx] + "\n") # do
+        self.idx += 1
+
+        self.output.write("</doStatement>\n")
+        return 0
+
+    def CompileLet(self): 
+        if not self.check():
+            return
+        self.output.write("<letStatement>\n")
+
+        self.output.write(self.input[self.idx] + "\n") # let
+        self.idx += 1
+        self.output.write(self.input[self.idx] + "\n") # varname
+        self.idx += 1
+        if getValue(self.input[self.idx]) == "[":
+            self.output.write(self.input[self.idx] + "\n") # [
+            self.idx += 1
+
+            #expression
+            self.CompileExpression()
+    
+            self.output.write(self.input[self.idx] + "\n") # ]
+            self.idx += 1
+        
+        self.output.write(self.input[self.idx] + "\n") # =
+        self.idx += 1
+        #expression
+        self.CompileExpression()
+        self.output.write(self.input[self.idx] + "\n") # ;
+        self.idx += 1
+
+        self.output.write("</letStatement>\n")
+        return 0
+
+    def CompileWhile(self): 
+        if not self.check():
+            return
+        self.output.write("<whileStatement>\n")
+
+        self.output.write(self.input[self.idx] + "\n") # while
+        self.idx += 1
+
+        self.output.write(self.input[self.idx] + "\n") # (
+        self.idx += 1
+        #expression
+        self.CompileExpression()
+        self.output.write(self.input[self.idx] + "\n") # )
+        self.idx += 1
+
+        self.output.write(self.input[self.idx] + "\n") # {
+        self.idx += 1
+        #statement
+        self.CompileStatements()
+        self.output.write(self.input[self.idx] + "\n") # }
+        self.idx += 1
+
+        self.output.write("</whileStatement>\n")
+        return 0
+
+    def CompileReturn(self): 
+        if not self.check():
+            return
+        self.output.write("<returnStatement>\n")
+
+        self.output.write(self.input[self.idx] + "\n") # return
+        self.idx += 1
+
+        if getValue(self.input[self.idx]) != ";": #expression? if not ";"
+            #expression
+            self.CompileExpression()
+
+        self.output.write(self.input[self.idx] + "\n") # ;
+        self.idx += 1
+
+        self.output.write("</returnStatement>\n")
+        return 0
+
+    def CompileIf(self): 
+        if not self.check():
+            return
+        
+        self.output.write("<ifStatement>\n")
+
+        self.output.write(self.input[self.idx] + "\n") # if
+        self.idx += 1
+
+        self.output.write(self.input[self.idx] + "\n") # (
+        self.idx += 1
+        #expression
+        self.CompileExpression()
+        self.output.write(self.input[self.idx] + "\n") # )
+        self.idx += 1
+
+        self.output.write(self.input[self.idx] + "\n") # {
+        self.idx += 1
+        #statement
+        self.CompileStatements()
+        self.output.write(self.input[self.idx] + "\n") # }
+        self.idx += 1
+
+        if getValue(self.input[self.idx]) == "else":
+            self.output.write(self.input[self.idx] + "\n") # {
+            self.idx += 1
+
+            #statement
+            self.CompileStatements()
+    
+            self.output.write(self.input[self.idx] + "\n") # }
+            self.idx += 1
+
+        self.output.write("</ifStatement>\n")
+        return 0
+
+    def CompileExpression(self): 
+        if not self.check():
+            return
+        self.output.write("<expression>\n")
+        self.CompileTerm()
+        self.output.write("</expression>\n")
+        return 0
+
+    def CompileTerm(self): 
+        if not self.check():
+            return
+        self.output.write("<term>\n")
+        self.output.write(self.input[self.idx] + "\n") # term
+        self.idx += 1
+        self.output.write("</term>\n")
+        return 0
+    
+    def CompaileExpressionList(self): 
+        if not self.check():
+            return
+        return 0
 
 if __name__ == '__main__':
     
@@ -154,30 +444,44 @@ if __name__ == '__main__':
     for input_file in source:
         tokenizer = JackTokenizer(input_file)
         # print(tokenizer.hasMoreTokens())
-        print("<tokens>")
+        tokenizer_output = []
+        # tokenizer_output.append("<tokens>")
+        # print("<tokens>")
         while tokenizer.hasMoreTokens():
             tokenizer.advance()
             if tokenizer.current_token != "":
                 # print(tokenizer.current_token)
+                # token_with_xml = ""
                 if tokenizer.tokenType() == "KEYWORD":
-                    print("<keyword> ",end="")
-                    print(tokenizer.keyWord(),end="")
-                    print(" </keyword>",end="")
+                    tokenizer_output.append("<keyword> {0} </keyword>".format(tokenizer.keyWord()))
                 if tokenizer.tokenType() == "SYMBOL":
-                    print("<symbol> ",end="")
-                    print(tokenizer.symbol(),end="")
-                    print(" </symbol>",end="")
+                    tokenizer_output.append("<symbol> {0} </symbol>".format(tokenizer.symbol()))
                 if tokenizer.tokenType() == "IDENTIFIER":
-                    print("<identifier> ",end="")
-                    print(tokenizer.identifier(),end="")
-                    print(" </identifier>",end="")
+                    tokenizer_output.append("<identifier> {0} </identifier>".format(tokenizer.identifier()))
                 if tokenizer.tokenType() == "INT_CONST":
-                    print("<integerConstant> ",end="")
-                    print(tokenizer.intVal(),end="")
-                    print(" </integerConstant>",end="")
+                    tokenizer_output.append("<integerConstant> {0} </integerConstant>".format(tokenizer.intVal()))
                 if tokenizer.tokenType() == "STRING_CONST":
-                    print("<stringConstant> ",end="")
-                    print(tokenizer.stringVal(),end="")
-                    print(" </stringConstant>",end="")
-                print("")
-        print("</tokens>")
+                    tokenizer_output.append("<stringConstant> {0} </stringConstant>".format(tokenizer.stringVal()))
+                # tokenizer_output.append(token_with_xml)
+        # tokenizer_output.append("</tokens>")
+        # print("\n".join(tokenizer_output)) #intermediate xml file
+
+
+        output_file = input_file[:-5] + ".xml"
+        ce = CompilationEngine(tokenizer_output,'cedev.xml')
+    
+
+
+        # while ce.idx < len(ce.input):
+            # print(ce.idx)
+
+        ce.CompileClass()
+            
+            # ce.idx += 1
+            
+
+
+
+        ce.close()
+        #     print(i + "yattaze")
+        # print(compilation_engine.output)
